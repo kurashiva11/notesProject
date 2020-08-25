d458a2a870365b971c41c6ba05c1638e7bd7fefb
kurashiva11 <kurashiva11@gmail.com>
2020-08-25 17:25:45 +0530

    don't know why heroku server is sending the previous js file to client even after running collect static in heroku server using heroku CLI

--- staticfiles/index.js
+++ staticfiles/index.js
@@ -1,4 +1,30 @@
-var AllowNotification = false;
+function getFiles(callback) {
+    $.ajax({
+        type: "get",
+        url: "getFiles",
+        headers: {'X-CSRFToken': $('input[name="csrfmiddlewaretoken"]').val()}, // for csrf token
+        success: function(data) {
+            callback(data);
+        },
+        error: function(e){
+            alert("something went wrong while retriving your files");
+            console.log(e)
+        }
+    });
+}
+
+function update_file_in_tree(files) {
+    $(".components").empty()
+    for (file of files) {
+        var file_struct = `<li>
+        <a href="#" class='file' id='${file.name}'>${file.name} <span style="float:right;"><span class="mr-1 fileDelete" id="${file.name}"><i class="fa fa-trash" aria-hidden="true"></i></span></span> </a>
+        </li>
+        <div class="custom-menu">
+        <li id="${file.name}" class="rename">rename</li>
+        </div>`
+        $(".components").append(file_struct)
+    }
+}
 
 $( function(e) {
     $('#sidebarCollapse').on('click', function () {
@@ -21,83 +47,79 @@ $( function(e) {
         }
     });
 
-    function get_files(){
-        var files = $(".files").text();
-        files = files.replace(/\"/g, '\\\"').replace(/\\\'/g, '`');
-        files = files.replace(/\'/g, '"').replace(/\`/g, "'");
-        files = JSON.parse(files)
-        return files;
-    }
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus');
+    })
 
-    function reloadFiles(data){
-        $(".files").text( data.filter(".files").text() );
-        return get_files();
-    }
+    if (is_authenticated) {
+        getFiles(function (data) {      // will get executed on succesful login.
+            update_file_in_tree(data);
+        });
 
-    $('.file').click(function() {
-        var name = $(this).text();
-        var files = get_files();
+        $('.save-file').click(function() {
+            var textarea = $(".notepad");
+            var filename = $(".fileName");
+            filename.val( filename.val().trim() );
 
-        for(var i=0; i<files.length; i++){
-            if(files[i]["name"].trim() == name.trim()){
-                var temp = files[i].text;
-                $('.notepad').val(temp);
-                $(".fileName").val(name);
-                break;
-            }
-        }
-    });
+            $(".save-file").html('<span class="spinner-border spinner-border-sm nav-link"></span>' + " Saving");
 
-    $(".fileDelete").click(function(e) {
-        e.stopPropagation();
-        var name = $(this).attr("id");
-
-        if(confirm("Are you sure you want to delete " + name + " permanently?")){
             $.ajax({
                 type: "post", // or "get"
-                url: "deleteFile",
-                data: {'name': name},
+                url: "addFile",
+                data: {'filename': filename.val(), 'filedata': textarea.val()},
                 headers: {'X-CSRFToken': $('input[name="csrfmiddlewaretoken"]').val()}, // for csrf token
                 success: function(data) {
-                    location.reload();
+                    $(".save-file").html("Save");
+                    getFiles(function (data) {
+                        update_file_in_tree(data);
+                    });
                 },
                 error: function(e){
                     alert("something went wrong");
                     location.reload();
                 }
             });
-        }
+        });
 
-    });
+        $(".components").on('click', '.fileDelete', function(e) {
+            e.stopPropagation();
+            var name = $(this).attr("id");
 
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus');
-    })
-
-    $('.save-file').click(function() {
-        var textarea = $(".notepad");
-        var filename = $(".fileName");
-        filename.val( filename.val().trim() );
-        var temp = textarea.val();
-        textarea.val(temp);
-        var files = get_files();
-        var files_len = files.length;
-
-        $(".save-file").html('<span class="spinner-border spinner-border-sm nav-link"></span>' + " Saving");
-
-
-        $.ajax({
-            type: "post", // or "get"
-            url: "addFile",
-            data: {'filename': filename.val(), 'filedata': textarea.val()},
-            headers: {'X-CSRFToken': $('input[name="csrfmiddlewaretoken"]').val()}, // for csrf token
-            success: function(data) {
-                location.reload();
-            },
-            error: function(e){
-                alert("something went wrong");
-                location.reload();
+            if(confirm("Are you sure you want to delete " + name + " permanently?")){
+                $.ajax({
+                    type: "post", // or "get"
+                    url: "deleteFile",
+                    data: {'name': name},
+                    headers: {'X-CSRFToken': $('input[name="csrfmiddlewaretoken"]').val()}, // for csrf token
+                    success: function(data) {
+                        getFiles(function (data) {
+                            update_file_in_tree(data);
+                        });
+                    },
+                    error: function(e){
+                        alert("something went wrong");
+                        location.reload();
+                    }
+                });
             }
         });
-    });
+
+        $(".components").on('click', '.file', function() {
+            var name = $(this).text();
+            console.log("clicked ", name);
+            getFiles(function (files) {
+                for (file of files) {
+                    if (file["name"].trim() == name.trim()) {
+                        $('.notepad').val(file["text"]);
+                        $(".fileName").val(name);
+                        break;
+                    }
+                }
+            });
+        });
+
+    } else {
+        console.log("not authenticated")
+        $("#loginModelLauncher").click();
+    }
 });
